a88ef5c03d9683627cac8c6f9f1367c60772e9eb
Derian Salvador Rodriguez Ramirez <49693491+croc2510@users.noreply.github.com>
2019-11-29 14:13:07 -0600

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,10 @@
+public class Ejercicio1{
+
+	
+	public static void main(String[] args){
+		int edad = 23;
+		String nombre = "Derian Rodriguez";
+		float peso = 80;
+		System.out.println(nombre+"\n"+edad+"\n"+peso);
+		}
+}
